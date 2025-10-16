46adffa179575a3413be465176d8356a68043154
edjulsin <edjulsin@gmail.com>
2025-10-16 20:55:20 +0700

    improve email template

--- email/Contact.tsx
+++ email/Contact.tsx
@@ -24,7 +24,7 @@ const Contact = ({ name, email, message }: { name: string, email: string, messag
         <Head />
         <Preview>Contact from Katt</Preview>
         <Tailwind>
-            <Body className="mx-auto my-auto bg-white px-2 font-sans">
+            <Body className="mx-auto my-auto bg-white font-sans">
                 <Container className="mx-auto my-[40px] max-w-[465px] rounded border border-[#eaeaea] border-solid p-[30px]">
                     <Section>
                         <Img
@@ -38,11 +38,11 @@ const Contact = ({ name, email, message }: { name: string, email: string, messag
                     <Heading className="mx-0 my-[30px] p-0 text-center font-normal text-[22px] text-neutral-800">
                         Contact from <strong>Katt</strong>
                     </Heading>
-                    <Text className="text-[14px] text-neutral-500 leading-[24px]">
+                    <Text className="text-[14px] text-neutral-500">
                         You've received a message through <strong>Katt's</strong> contact form.
                     </Text>
-                    <Section className="text-neutral-500 leading-[24px]">
-                        <Row className='my-[20px]'>
+                    <Section className="text-neutral-500 my-[30px]">
+                        <Row className='mb-[20px]'>
                             <Column>
                                 <Text className='m-0 p-0 text-[12px] mb-[5px] text-neutral-400'>Name</Text>
                                 <Text className='capitalize m-0 p-0 text-[14px]'>{ name }</Text>
@@ -54,14 +54,13 @@ const Contact = ({ name, email, message }: { name: string, email: string, messag
                                 <Link className='m-0 p-0 underline text-[14px] leading-[24px]' href={ `mailto:${email}` }>{ email }</Link>
                             </Column>
                         </Row>
-                        <Row className='my-[20px]'>
+                        <Row className='mt-[20px]'>
                             <Column>
                                 <Text className='m-0 p-0 text-[12px] mb-[5px] text-neutral-400'>Message</Text>
                                 <Text className='m-0 p-0 text-[14px]'>{ message }</Text>
                             </Column>
                         </Row>
                     </Section>
-                    <br />
                     <Hr className="mx-0 my-[5px] w-full border border-[#eaeaea] border-solid" />
                     <Text className="text-neutral-400 text-[12px] leading-[24px]">
                         This is an automated message. Please reply directly to the sender's email.
--- email/Login.tsx
+++ email/Login.tsx
@@ -19,7 +19,7 @@ const Login = ({ confirm, logo }: { confirm: string, logo: string }) =>
         <Head />
         <Preview>Login to Katt</Preview>
         <Tailwind>
-            <Body className="mx-auto my-auto bg-white px-2 font-sans">
+            <Body className="mx-auto my-auto bg-white font-sans">
                 <Container className="mx-auto my-[40px] max-w-[465px] rounded border border-[#eaeaea] border-solid p-[30px]">
                     <Section>
                         <Img
@@ -30,28 +30,27 @@ const Login = ({ confirm, logo }: { confirm: string, logo: string }) =>
                             className="mx-auto my-0 w-[200px] h-auto"
                         />
                     </Section>
-                    <Heading className="mx-0 my-[30px] p-0 text-center font-normal text-[24px] text-black">
+                    <Heading className="mx-0 my-[30px] p-0 text-center font-normal text-[22px] text-black">
                         Login to <strong>Katt</strong>
                     </Heading>
-                    <Text className="text-[14px] leading-[24px] text-neutral-500">
+                    <Text className="text-[14px] my-[40px] text-neutral-500">
                         Click one of the options below to login to <strong>Katt's</strong> dashboard.
                     </Text>
-                    <Section className="mt-[32px] mb-[16px] text-center">
+                    <Section className="my-[30px] text-center">
                         <Button
                             className="rounded bg-black px-4 py-2 text-center font-semibold text-[12px] text-white no-underline"
                             href={ confirm }
                         >
                             Dashboard
                         </Button>
+                        <Text className='text-center text-[14px] leading-[24px] text-neutral-400'>or</Text>
+                        <Text className="text-[14px] text-blue-500 leading-[24px] text-center">
+                            <Link href={ confirm } className="underline">
+                                Magic link
+                            </Link>
+                        </Text>
                     </Section>
-                    <Text className='text-center text-[14px] leading-[24px] text-neutral-400'>or</Text>
-                    <Text className="text-[14px] text-blue-500 leading-[24px] text-center">
-                        <Link href={ confirm } className="underline">
-                            Magic link
-                        </Link>
-                    </Text>
-                    <br />
-                    <Hr className="mx-0 my-[26px] w-full border border-[#eaeaea] border-solid" />
+                    <Hr className="mx-0 my-[5px] w-full border border-[#eaeaea] border-solid" />
                     <Text className="text-neutral-400 text-[12px] leading-[24px]">
                         If you didn't try to login, you can safely ignore this email.
                     </Text>

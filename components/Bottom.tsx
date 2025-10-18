524798c90292d6223d49b9415221eb595e9fc545
edjulsin <edjulsin@gmail.com>
2025-10-19 03:17:16 +0700

    improve bottom copy and gap

--- components/Bottom.tsx
+++ components/Bottom.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link';
 
 const Bottom = ({ copy }: { copy?: string }) =>
-    <section className='text-center flex flex-col gap-y-15 max-w-2xs md:max-w-sm lg:max-w-md'>
-        <h3 className='text-xl/loose lg:text-2xl/loose font-serif'>{ copy || 'Let’s plan your space' }</h3>
+    <section className='text-center flex flex-col gap-y-10 max-w-2xs md:max-w-sm lg:max-w-md'>
+        <h3 className='text-xl/loose lg:text-2xl/loose font-serif'>{ copy || 'Design your space with us' }</h3>
         <Link className='text-lg font-semibold font-sans text-amber-600' href='/contact'>Contact us &rarr;</Link>
     </section>
 

1a80826a8f81c9db7e49e35f5ce4e1d8a82505b5
edjulsin <edjulsin@gmail.com>
2025-10-23 03:22:37 +0700

    fix color during darkmode

--- app/maintenance/page.tsx
+++ app/maintenance/page.tsx
@@ -23,7 +23,7 @@ const MaintenancePage = () =>
             <main className='flex flex-col gap-y-40 justify-center items-center *:w-full'>
                 <section role='alert' className='h-[100dvh] w-full flex flex-col justify-center items-center gap-y-15 text-center max-w-xs sm:max-w-lg md:max-w-2xl lg:max-w-4xl xl:max-w-6xl font-medium'>
                     <h1 className='text-3xl/relaxed sm:text-6xl/relaxed md:text-7xl/relaxed lg:text-8xl/relaxed xl:text-9xl/relaxed font-serif'>Our site is under maintenance</h1>
-                    <p className='font-sans text-lg/relaxed sm:text-xl/relaxed md:text-2xl/relaxed lg:text-3xl/relaxed xl:text-4xl/relaxed text-neutral-500'>We should be back shortly. Thank you for your patience.</p>
+                    <p className='font-sans text-lg/relaxed sm:text-xl/relaxed md:text-2xl/relaxed lg:text-3xl/relaxed xl:text-4xl/relaxed text-gold-950'>We should be back shortly. Thank you for your patience.</p>
                 </section>
             </main>
         </div>
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ const NotFound = () =>
         <main className='flex flex-col gap-y-40 justify-center items-center *:w-full'>
             <section role='alert' className='h-[100dvh] w-full flex flex-col justify-center items-center gap-y-15 text-center max-w-xs sm:max-w-lg md:max-w-2xl lg:max-w-4xl xl:max-w-6xl font-medium'>
                 <h1 className='text-3xl/relaxed sm:text-6xl/relaxed md:text-7xl/relaxed lg:text-8xl/relaxed xl:text-9xl/relaxed font-serif'>Oops, that page is gone!</h1>
-                <p className='font-sans text-lg/relaxed sm:text-xl/relaxed md:text-2xl/relaxed lg:text-3xl/relaxed xl:text-4xl/relaxed text-neutral-500'>The page you’re looking for doesn’t exist or has been moved.</p>
+                <p className='font-sans text-lg/relaxed sm:text-xl/relaxed md:text-2xl/relaxed lg:text-3xl/relaxed xl:text-4xl/relaxed text-gold-950'>The page you’re looking for doesn’t exist or has been moved.</p>
                 <Link className='font-sans text-lg/loose sm:text-xl/loose text-amber-600 font-semibold' href='/'>Back home &rarr;</Link>
             </section>
         </main>

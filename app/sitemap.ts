e131b8035afe66caf92292ac45c4bf6670cbb9eb
edjulsin <edjulsin@gmail.com>
2025-10-26 05:42:46 +0700

    switch to supabase admin for generating sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,6 @@
-import { getAllPublishedProjects } from '@/action/server'
+import { getAllPublishedProjects } from '@/action/admin'
 import { Project } from '@/type/editor'
 import type { MetadataRoute } from 'next'
-
-export const revalidate = 604800;
 
 const development = process.env.NODE_ENV === 'development'
 const url = (development ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_SITE_URL) as string
@@ -65,7 +63,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
                     images: v.assets.map(v => v.src)
                 }
             })
-            return [ ...statics, ...result ]
+            return [...statics, ...result]
         },
         () => statics
     )

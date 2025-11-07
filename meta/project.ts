3e674a48446e5e4a9e748517fe2abec3eb2f7ee3
edjulsin <edjulsin@gmail.com>
2025-11-07 23:28:06 +0700

    fix project  meta canonical

--- meta/project.ts
+++ meta/project.ts
@@ -8,7 +8,7 @@ const url = development ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_SITE
 const projectMeta = (v: Project): Metadata => ({
     title: `${v.title} | By ${name}`,
     description: v.description,
-    alternates: { canonical: `${url}/${v.slug}` },
+    alternates: { canonical: `${url}/projects/${v.slug}` },
     openGraph: {
         type: 'article',
         siteName: name,
